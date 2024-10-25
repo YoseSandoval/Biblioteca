bac51ac6f279dc338c879877939c41f3b76deb54
Yoselin Sandoval <yjsandoval55@gmail.com>
2024-10-25 00:21:06 -0600

    Cambios en el archivo configuración

--- Config/Config.php
+++ Config/Config.php
@@ -1,4 +1,5 @@
 <?php
+const base_url = "https://grupo8-app-df15ea3cb69c.herokuapp.com/";
 const host = "k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 const user = "f1zzu2f38q6qeakk";
 const pass = "ygllllj3cxo2cn45";
